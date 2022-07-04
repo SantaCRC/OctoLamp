31c684f15e9943faa143362460a7d11042a1dbd7
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2022-07-04 13:15:27 -0600

    id

--- octoprint_OctoLamp/static/js/OctoLamp.js
+++ octoprint_OctoLamp/static/js/OctoLamp.js
@@ -38,6 +38,6 @@ $(function() {
         ["settingsViewModel"],
 
         // Finally, this is the list of selectors for all elements we want this view model to be bound to.
-        ["#settings_plugin_OctoLamp"]
+        [document.getElementById("settings_plugin_OctoLamp")]
     ]);
 });
