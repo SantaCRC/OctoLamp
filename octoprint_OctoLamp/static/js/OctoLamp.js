5a8cc65fb49e557940fcdaac856f737f85d4b417
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2022-07-04 01:21:55 -0600

    js

--- octoprint_OctoLamp/static/js/OctoLamp.js
+++ octoprint_OctoLamp/static/js/OctoLamp.js
@@ -7,6 +7,39 @@
 $(function() {
     function OctolampViewModel(parameters) {
         var self = this;
+        self.settings = parameters[0];
+        self.username = ko.observable();
+        self.password = ko.observable();
+
+        self.login = function() {
+            var username = self.username();
+            var password = self.password();
+
+            $.ajax({
+                url: API_BASEURL + "octolamp/login",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({
+                    username: username,
+                    password: password
+                }),
+                contentType: "application/json; charset=UTF-8",
+                success: function(data) {
+                    showMessage(gettext("Login successful"), "success");
+                },
+                error: function(xhr) {
+                    if (xhr.status == 401) {
+                        showMessage(gettext("Invalid username or password."), "error");
+                    } else {
+                        showMessage(gettext("Could not contact OctoLamp."), "error");
+                    }
+                }
+
+
+            });   
+
+            
+        }
 
         // assign the injected parameters, e.g.:
         // self.loginStateViewModel = parameters[0];
@@ -22,8 +55,8 @@ $(function() {
     OCTOPRINT_VIEWMODELS.push({
         construct: OctolampViewModel,
         // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        dependencies: ["settingsViewModel"],
         // Elements to bind to, e.g. #settings_plugin_OctoLamp, #tab_plugin_OctoLamp, ...
-        elements: [ /* ... */ ]
+        elements: ["settings_plugin_OctoLamp"]
     });
 });
