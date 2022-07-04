67ef41bf7070a3651f0cc0d7ee4ad3b2622fb86e
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2022-07-03 23:34:29 -0600

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "octolamp"
+plugin_identifier = "OctoLamp"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_OctoLamp"
