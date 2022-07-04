ee09cde06851db4d9c7521f3bc51ae67fece855e
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2022-07-04 01:54:03 -0600

    test connection fix

--- octoprint_OctoLamp/static/js/OctoLamp.js
+++ octoprint_OctoLamp/static/js/OctoLamp.js
@@ -7,12 +7,26 @@
 $(function() {
     function OctolampViewModel(parameters) {
         var self = this;
+        self.settings = parameters[0];
+        self.username = ko.observable();
+        self.password = ko.observable();
+
+        self.testconnection = function() {
+            var username = self.username();
+            var password = self.password();
+            alert(username + " " + password);
+
+            
+        }
 
         // assign the injected parameters, e.g.:
         // self.loginStateViewModel = parameters[0];
         // self.settingsViewModel = parameters[1];
 
         // TODO: Implement your plugin's view model here.
+        // self.onBeforeBinding = function() {
+        //     self.settings = self.settingsViewModel.settings;
+        // };
     }
 
     /* view model class, parameters for constructor, container to bind to
@@ -22,8 +36,8 @@ $(function() {
     OCTOPRINT_VIEWMODELS.push({
         construct: OctolampViewModel,
         // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        dependencies: ["settingsViewModel"],
         // Elements to bind to, e.g. #settings_plugin_OctoLamp, #tab_plugin_OctoLamp, ...
-        elements: [ /* ... */ ]
+        elements: ["settings_plugin_OctoLamp"]
     });
 });
