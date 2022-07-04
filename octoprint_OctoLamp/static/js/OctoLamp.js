f95fb5453ff414c26a3fe77a02abf480ce061a57
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2022-07-04 11:38:50 -0600

    bindings true

--- octoprint_OctoLamp/static/js/OctoLamp.js
+++ octoprint_OctoLamp/static/js/OctoLamp.js
@@ -37,6 +37,6 @@ $(function() {
         ["settingsViewModel"],
 
         // Finally, this is the list of selectors for all elements we want this view model to be bound to.
-        ["#tab_plugin_helloworld"]
+        ["#settings_plugin_octolamp"]
     ]);
 });
