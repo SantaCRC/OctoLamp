7d6445a4ac9b1bf4246b649bdff0727d7a8017c7
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2022-07-04 03:25:24 -0600

    test

--- octoprint_OctoLamp/static/js/OctoLamp.js
+++ octoprint_OctoLamp/static/js/OctoLamp.js
@@ -7,38 +7,43 @@
 $(function() {
     function OctolampViewModel(parameters) {
         var self = this;
+
         self.settings = parameters[0];
 
+        // this will hold the URL currently displayed by the iframe
+        self.currentUrl = ko.observable();
+
+        // this will hold the URL entered in the text field
+        self.newUrl = ko.observable();
+
+        // this will be called when the user clicks the "Go" button and set the iframe's URL to
+        // the entered URL
         self.test = function() {
-            var username = self.settings.username();
-            var password = self.settings.password();
-            alert(username + " " + password);
+            self.currentUrl(self.newUrl());
         };
 
+        // This will get called before the HelloWorldViewModel gets bound to the DOM, but after its
+        // dependencies have already been initialized. It is especially guaranteed that this method
+        // gets called _after_ the settings have been retrieved from the OctoPrint backend and thus
+        // the SettingsViewModel been properly populated.
         self.onBeforeBinding = function() {
-            self.username(self.settings.username);
-            self.password(self.settings.password);
-        };
-
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
-
-        // TODO: Implement your plugin's view model here.
-        // self.onBeforeBinding = function() {
-        //     self.settings = self.settingsViewModel.settings;
-        // };
+            self.newUrl(self.settings.settings.plugins.helloworld.url());
+            self.goToUrl();
+        }
     }
 
-    /* view model class, parameters for constructor, container to bind to
-     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
-     * and a full list of the available options.
-     */
-    OCTOPRINT_VIEWMODELS.push({
-        construct: OctolampViewModel,
-        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: ["settingsViewModel"],
-        // Elements to bind to, e.g. #settings_plugin_OctoLamp, #tab_plugin_OctoLamp, ...
-        elements: ["#OctoLampSettings"]
-    });
+    // This is how our plugin registers itself with the application, by adding some configuration
+    // information to the global variable OCTOPRINT_VIEWMODELS
+    OCTOPRINT_VIEWMODELS.push([
+        // This is the constructor to call for instantiating the plugin
+        OctolampViewModel,
+
+        // This is a list of dependencies to inject into the plugin, the order which you request
+        // here is the order in which the dependencies will be injected into your view model upon
+        // instantiation via the parameters argument
+        ["settingsViewModel"],
+
+        // Finally, this is the list of selectors for all elements we want this view model to be bound to.
+        ["#tab_plugin_helloworld"]
+    ]);
 });
